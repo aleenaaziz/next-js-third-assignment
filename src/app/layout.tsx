fa0bf5dc09422fc49f43a98a3799c25cdf59e5aa
alina <alinaaziz831@gmail.com>
2024-12-24 02:00:07 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,35 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="flex justify-center space-x-4">
+          <Link href="/">
+            <li className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+          <li
+            className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          >
+            about
+          </li>
+          </Link>
+          <Link href="/project">
+          <li
+            
+            className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          >
+            Projects
+          </li></Link>
+          <Link href="/footer">
+          <li
+            
+            className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          >
+            Footer
+          </li>
+          </Link>
+        </nav>
         {children}
       </body>
     </html>
